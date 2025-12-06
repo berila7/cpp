3959364c1f92570832d495dc24073fe800a6abae
rdp9116 <rdp9116@gmail.com>
2025-12-06 23:01:10 +0000

    complete the ex01

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int					_value;
+	static int const	_fractionalBits = 8;
+public:
+	Fixed();
+	Fixed( int const num );
+	Fixed( float const num );
+	Fixed( const Fixed& obj );
+	~Fixed();
+	
+	Fixed&	operator=( const Fixed& src);
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
+
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
